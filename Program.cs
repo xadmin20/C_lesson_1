48be21dc4e01262ad0640db177f18612369a76f5
xadmin20 <xadmin@bk.ru>
2022-08-20 13:46:23 +0300

    Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите  2(два) числа ");
+int x=int.Parse(Console.ReadLine());
+int y=int.Parse(Console.ReadLine());
+if (x>y) {
+    Console.Write("Max = " +x);
+}
+else Console.Write("Max = " +y);
+// ##############################################################################
